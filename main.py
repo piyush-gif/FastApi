91739b3e80dfb07a5b5ea4ca4d5898e79de193ce
piyush <piyush.khadka2233@gmail.com>
2026-03-07 20:07:24 +0000

    simple post register req

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import httpx
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello":"World"}
+
+class RegisterRequest(BaseModel):
+  username: str
+  email: str
+  passwrod: str
 
 
-@app.get("/pokemon/{name}")
-async def pokemon_info(name: str):
-  async with httpx.AsyncClient() as client:
-    response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-    data = response.json()
-
-    return {
-      "id" : data["id"],
-      "sprite":data["sprites"]["front_default"],
-      "name" : data["name"],
-      "height" : data["height"],
-    }
-  
-
-@app.get("/pokemon/")
-async def pokemon_list(limit: int = 20, offset: int = 0):
-  async with httpx.AsyncClient() as client:
-    response = await client.get(f"https://pokeapi.co/api/v2/pokemon/?limit={limit}&offset={offset}")
-    data = response.json()
-    return data
-  
-
-
-@app.get("/pokemon/{name}/moves")
-async def pokemon_moves(name : str):
-  async with httpx.AsyncClient() as client:
-    response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-    data = response.json()
-    return {"moves" : [m["move"]["name"] for m in data["moves"]]}
+@app.post("post")
+def register(user: RegisterRequest):
+  return {
+    "message" : "Registered",
+    "username" : user.username
+  }
